10074486a10f8f8c326bb52850f29086faeb5ebe
hungmktcoll <hungmktcoll@gmail.com>
2020-11-05 23:52:32 +0700

    Migration Database

--- eShopSolution.Data/EF/eShopDbContext.cs
+++ eShopSolution.Data/EF/eShopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using eShopSolution.Data.Entities;
+﻿using eShopSolution.Data.Configurations;
+using eShopSolution.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,10 +7,24 @@ using System.Text;
 
 namespace eShopSolution.Data.EF
 {
-    public class eShopDbContext : DbContext
+    public class EShopDbContext : DbContext
     {
-        public eShopDbContext(DbContextOptions options) : base(options)
+        public EShopDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            //base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<Product> Products { get; set; }
@@ -18,12 +33,12 @@ namespace eShopSolution.Data.EF
         public DbSet<AppConfig> AppConfigs { get; set; }
 
 
-        public DbSet<Cart> Carts { get; set; }
+        //public DbSet<Cart> Carts { get; set; }
 
         public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
         public DbSet<ProductInCategory> ProductInCategories { get; set; }
 
-        public DbSet<Contact> Contacts { get; set; }
+        //public DbSet<Contact> Contacts { get; set; }
 
         public DbSet<Language> Languages { get; set; }
 
@@ -32,9 +47,9 @@ namespace eShopSolution.Data.EF
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<ProductTranslation> ProductTranslations { get; set; }
 
-        public DbSet<Promotion> Promotions { get; set; }
+        //public DbSet<Promotion> Promotions { get; set; }
 
 
-        public DbSet<Transaction> Transactions { get; set; }
+        //public DbSet<Transaction> Transactions { get; set; }
     }
 }
--- eShopSolution.Data/Entities/Language.cs
+++ eShopSolution.Data/Entities/Language.cs
@@ -11,5 +11,8 @@ namespace eShopSolution.Data.Entities
         public string Name { get; set; }
 
         public bool IsDefault { get; set; }
+
+        public List<CategoryTranslation> CategoryTranslations { get; set; }
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }
